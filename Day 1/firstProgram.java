f0b7df718baaf6fbe35902b9bb6a5b167735f056
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-03-09 21:59:10 +0530

    Day 1 Completed

---
+++ Day 1/firstProgram.java
@@ -0,0 +1,8 @@
+class firstProgeam{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.print("Saurabh Kumawat\n");
+        System.out.println("356 Days Of Code Challenge");
+        System.out.print("Day 1");
+    }
+}
