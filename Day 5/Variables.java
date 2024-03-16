f91a41019630c10778d0754eed13d56dfe55e61a
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-03-16 23:58:31 +0530

    Day 5 Completed

---
+++ Day 5/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Saurabh";
+        System.out.println(name);
+
+        b = 105;
+        System.out.println(b);
+
+        name = "Saurabh Kumawat";
+        System.out.println(name);
+    }
+    
+    
+}
