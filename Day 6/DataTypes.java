eaf5de2388284d959eb87a5d1ecaccf3fcbd988a
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-03-19 18:10:02 +0530

    Day 6 Completed

---
+++ Day 6/DataTypes.java
@@ -0,0 +1,45 @@
+public class DataTypes{
+    public static void main(String args[]){
+        System.out.println("Day 6\nData Types\n");
+        System.out.println("Primitive Data Types");
+        // byte
+        byte b = -128;
+        System.out.println(b);
+        System.out.println("Byte size is 1 byte");
+        // short
+        short s = 333;
+        System.out.println(s);
+        System.out.println("Short size is 2 bytes");
+
+
+        // char 
+        char c = 'a';
+        System.out.println(c);
+        System.out.println("char size is 2 byte");
+
+        // boolean = 1bit
+        boolean t = true;
+        boolean f = false;
+        System.out.println(t);
+        System.out.println(f);
+
+
+        // int  = 4 byte
+        int i = 100;
+        System.out.println(i);
+
+
+        // long = 8 byte
+        long l = 465464;
+        System.out.println(l);
+
+
+        // float = 4byte
+        float ff = 10.5f;
+        System.out.println(ff);
+
+        // double = 8byte
+        double d = 4.55d;
+        System.out.println(d);
+}
+}
