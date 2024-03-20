cf2b54860941669a45c39675eac44505dc5e84b5
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-03-20 21:49:44 +0530

    DAy 7 Completed

---
+++ Day 7/Input.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        // String in = sc.next();
+        // System.out.println(in);
+
+        // big string capture
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        // for integer
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+
+        // for float 
+        // Float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // for byte
+        // Byte smNum = sc.nextByte();
+        // System.out.println("value is " + smNum);
+
+
+        // for double 
+        // Double db = sc.nextDouble();
+        // System.out.println(db);
+
+        // for boolean 
+    //     Boolean isLoggedIn = sc.nextBoolean();
+    //     System.out.println(isLoggedIn);
+
+        // for short
+        // Short num = sc.nextShort();
+        // System.out.println(num);
+
+        // for long
+        // Long bigNum = sc.nextLong();
+        // System.out.println(bigNum);
+
+
+    }
+}
