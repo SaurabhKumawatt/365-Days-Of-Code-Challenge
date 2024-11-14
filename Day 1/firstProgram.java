6defef06ce32bc9c562a188d4f96a0652a8cab75
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2024-11-14 22:57:21 +0530

    updated

--- Day 1/firstProgram.java
+++
@@ -1,8 +0,0 @@
-class firstProgeam{
-    public static void main(String args[]){
-        System.out.println("Hello World!");
-        System.out.print("Saurabh Kumawat\n");
-        System.out.println("356 Days Of Code Challenge");
-        System.out.print("Day 1");
-    }
-}
