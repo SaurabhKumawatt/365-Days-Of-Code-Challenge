bafc553b578655a77768d945680ffca862f85171
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2024-11-17 21:41:02 +0530

    Day 2 Completed

---
+++ Day 2/casting.java
@@ -0,0 +1,17 @@
+
+
+public class casting {
+    public static void main(String[] args) {
+        // casting 
+
+        // implicit casting
+        double price = 100.00;
+        double fPrice = price + 18.00;
+        System.out.println(fPrice);
+
+        // explicit casting
+        int p = 100;
+        int fP = p + (int)18.0;
+        System.out.println(fP);
+    }
+}
