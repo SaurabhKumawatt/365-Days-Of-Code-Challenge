7594e18bdf403d5e02e36531ad88a9c379de0b3a
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2024-11-20 23:13:51 +0530

    Day 3 Completed

---
+++ Day 3/Math.java
@@ -0,0 +1,20 @@
+public class Math {
+    
+    public static void main(String[] args){
+
+        // math class
+        
+        // min value
+        System.out.println(java.lang.Math.min(5, 6));
+
+        // max value
+        System.out.println(java.lang.Math.max(5, 6));
+
+        // random generator
+        System.out.println((int)(java.lang.Math.random()*100));
+
+
+
+    }
+
+}
